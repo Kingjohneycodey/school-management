d1c3fb298c307458eadfac577383a822aa3a3c03
Kingjohneycodey <kvngjohnny10@gmail.com>
2024-08-19 22:28:25 +0100

    Working on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,35 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <header className="flex items-center justify-between">
+    <main className="home-page">
+      <header className="flex items-center justify-between p-4">
         <img src="/images/logo.svg" alt="" />
 
         <div className="">
-          <Link href="/about">Home</Link>
+          <Link href="/" className="mr-12">
+            Home
+          </Link>
+          <Link href="/about" className="mr-12">
+            About
+          </Link>
+          <Link href="/pricing">Pricing</Link>
         </div>
 
         <div>
-          <button>Get Started</button>
+          <button className="rounded-2xl px-4 py-2 bg-[#B9FF66] text-black">
+            Get Started
+          </button>
         </div>
       </header>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>School dashboard</h1>
-      </div>
+
+
+      <section className="flex min-h-screen flex-col items-center p-24 custom-heading">
+       <div className="mb-10">
+       <h1>School Management Dashboard</h1>
+       </div>
+
+        <Image src="/images/banner.svg" width={100} height={100} className="w-full" alt="Cover picture" />
+      </section>
     </main>
   );
 }
