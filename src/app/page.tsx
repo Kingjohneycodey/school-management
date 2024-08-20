38562cb54b9394abe3eb419a1ee8772165949729
Kingjohneycodey <kvngjohnny10@gmail.com>
2024-08-20 05:56:07 +0100

    Added footer to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="home-page">
       <header className="fixed left-0 inset-x-0 top-0 z-50 bg-black">
         <nav
-          className="flex items-center justify-between p-6 lg:px-8"
+          className="flex items-center justify-between p-6 px-2 lg:px-8"
           aria-label="Global"
         >
           <Dialog.Root open={open} onOpenChange={setOpen}>
@@ -28,7 +28,7 @@ export default function Home() {
                 <HamburgerMenuIcon className="h-6 w-6" aria-hidden="true" />
               </button>
             </Dialog.Trigger>
-            <Dialog.Content className="fixed inset-0 z-50 bg-white p-6">
+            <Dialog.Content className="fixed inset-0 z-50 bg-[var(--secondary)] p-6">
               <button
                 onClick={() => setOpen(false)}
                 className="absolute top-4 right-4"
@@ -40,7 +40,7 @@ export default function Home() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="text-base font-semibold leading-7 text-gray-900"
+                    className="text-base font-semibold leading-7 text-[var(--tertiary)]"
                   >
                     {item.name}
                   </a>
@@ -48,12 +48,19 @@ export default function Home() {
               </div>
             </Dialog.Content>
           </Dialog.Root>
-          <div className="flex lg:flex-1">
+
+          {/* Logo centered on mobile */}
+          <div className="flex flex-1 justify-center lg:justify-start lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5 flex items-center gap-3">
-              <img src="/images/logo.svg" alt="" className="max-w-[200px] md:max-w-[250px]" />
+              <img
+                src="/images/logo.svg"
+                alt=""
+                className="max-w-[200px] md:max-w-[250px]"
+              />
             </a>
           </div>
 
+          {/* Desktop navigation links */}
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
               <a
@@ -65,6 +72,8 @@ export default function Home() {
               </a>
             ))}
           </div>
+
+          {/* Get Started button */}
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <Link
               href="/login"
@@ -78,26 +87,20 @@ export default function Home() {
         </nav>
       </header>
 
-      {/* <header className="flex items-center justify-between p-8 px-10">
-        <img src="/images/logo.svg" alt="" className="max-w-[250px]" />
-
-
-      </header> */}
-
       <section className="flex min-h-screen flex-col items-center p-4 md:p-24 mt-24 pt-16">
         <div className="mb-16 text-center">
           <h1 className="custom-heading text-[#B9FF66]">
             School Management Dashboard
           </h1>
 
-          <p className="max-w-[70%] m-auto mt-8">
+          <p className="md:max-w-[70%] m-auto mt-8">
             A School Management SaaS product designed to manage student,
             Teacher, and Parent activities, enabling effective management of the
             school and learning resources.
           </p>
 
           <p className="mt-12">
-          <Link
+            <Link
               href="/login"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
@@ -116,6 +119,11 @@ export default function Home() {
           alt="Cover picture"
         />
       </section>
+
+
+      <footer className="text-center mb-8">
+        School Admin Dashboard 2024
+      </footer>
     </main>
   );
 }
